428839af89066e76f173a2cbca2f1b9a994cf7de
Yueyu Hoshizora <yueyuhoshizora@gmail.com>
2023-03-10 10:49:35 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,5 @@
 import os, sys, getopt, json
-import configparser
+import configparser, uuid
 import openai
 
 def main(argv):
@@ -14,41 +14,50 @@ def main(argv):
         print('chatbot.py -m <message> -u <user>')
         sys.exit(2)
     
+    userhash = None
+
     for opt, arg in opts:
         if opt == '-h':
             print('chatbot.py -m <message> -u <user>')
             sys.exit(0)
         elif opt in ("-m", "--message"):
-            message: str = arg
+            inputmsg: str = arg
         elif opt in ("-u", "--user"):
             userhash: str = arg
+
+    # 沒有使用者, 則產生隨機
+    if userhash == None:
+        userhash = str(uuid.uuid4())
 
     # 設定 OpenAI KEY
     openai.api_key = cf.get("openai", "OPENAI_API_KEY")
     openai.organization = cf.get("openai", "OPENAI_ORGANIZATION_ID")
-
-    # 設定要傳送的訊息
-    context = message
-
+    
     # 創建 OpenGPT 3.5 的模型
     model_engine = "gpt-3.5-turbo"
     #model_engine = "text-davinci-003"
-    
-    # 獲取模型回應
-    response = openai.Completion.create(
-        engine=model_engine,
-        user=userhash, # 設置使用者 ID
-        prompt=context,
-        temperature=0.7,
-        max_tokens=4080,
-        stop=None,
-    )
 
-    # 獲取模型生成的內文
-    result = response.choices[0].text.strip()
+    # 定義 OpenAI GPT 請求
+    def generate_response(prompt, user):
+        response = openai.Completion.create(
+            engine=model_engine,
+            prompt=prompt,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.7,
+            presence_penalty=0,
+            frequency_penalty=0,
+            user=user  # 區別使用者
+        )
+        message = response.choices[0].text
+        return message.strip()
 
-    # CLI 列印回應結果
-    print(result)
+    # 取得請求結果
+    message = generate_response(inputmsg, userhash)
+
+    # CLI 請求結果
+    print(message)
 
     # TODO: 處理上下文關係表
 
